3c1ab5f92bc87e8f3ed120be37e7e496d00983f5
Megaju <julien_malle@outlook.fr>
2016-11-04 10:49:13 +0100

    ajout prototype pour nos perso

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // création de notre tout 1er personnage
-var perso = {
+/*var perso = {
     name: "Aurora",
     life: 150,
     force: 25,
@@ -10,10 +10,10 @@ var perso = {
         var description = this.name + " a " + this.life + " points de vie et a une force de " + this.force + " et possède " + this.xp + " d'expérience.";
         return description;
     }
-};
+};*/
 
 // compagnon d'Aurora
-var perso2 = {
+/*var perso2 = {
     nom: "Glacius",
     sante: 130,
     force: 30,
@@ -24,14 +24,41 @@ var perso2 = {
         var description = this.name + " a " + this.life + " points de vie et a une force de " + this.force + " et possède " + this.xp + " d'expérience.";
         return description;
     }
+};*/
+
+var Perso = {
+    name: "",
+    life: 0,
+    force: 0,
+    xp: 0,
+
+    // Renvoie la description du personnage
+    describe: function () {
+        var description = this.name + " a " + this.life + " points de vie, " +
+            this.force + " en force et " + this.xp + " points d'expérience";
+        return description;
+    }
 };
 
-console.log(perso.describe());
+var perso1 = Object.create(Perso);
+perso1.nom = "Aurora";
+perso1.sante = 150;
+perso1.force = 25;
+
+var perso2 = Object.create(Perso);
+perso2.nom = "Glacius";
+perso2.sante = 130;
+perso2.force = 35;
+
+console.log(perso1.describe());
+console.log(perso2.describe());
+
+console.log(perso1.describe());
 
 // Aurora est blessée par une flèche
-perso.life = perso.life - 20;
+perso1.life = perso1.life - 20;
 
 // Aurora trouve un bracelet de force
-perso.force = perso.force + 10;
+perso1.force = perso1.force + 10;
 
-console.log(perso.describe());
+console.log(perso1.describe());
